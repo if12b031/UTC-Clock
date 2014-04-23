4cc1484afe9ae1e6b881c7a94bfe04b82ca94c48
if12b103 <georg.huszar@gmail.com>
2014-04-23 13:35:55 +0200

    added Handlers for MySquare(working), MyCircle(not working)

--- Graphics Editor/src/objects/MyCircle.java
+++ Graphics Editor/src/objects/MyCircle.java
@@ -2,7 +2,10 @@ package objects;
 
 import interfaces.Drawable;
 import interfaces.IShape;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 
@@ -30,8 +33,45 @@ public class MyCircle extends Circle implements IShape,Drawable,Cloneable {
 
 	@Override
 	public void setHandler() {
-		// TODO Auto-generated method stub
+		this.setOnMouseClicked(new EventHandler<MouseEvent>(){
+		 
+		            @Override
+		            public void handle(MouseEvent event) {
+		            	if(event.getButton().toString().equals("SECONDARY")){
+		            		
+		            	}else{
+			            	if(MyCircle.this.isFocused()){
+			            		MyCircle.this.setStrokeWidth(0);
+			            		MyCircle.this.setFocused(false);
+			            	}else{
+			            		MyCircle.this.setStroke(new Color(1, 0, 0, 1));
+			            		MyCircle.this.setStrokeWidth(2);
+			            		MyCircle.this.setFocused(true);
+			            	}
+		            	}
+		            	
+		            	System.out.println("Rectangle OnClick");
+		            	
+		            }
+		        });
 		
+		this.setOnMouseDragged(new EventHandler<MouseEvent>(){
+		 
+		            @Override
+		            public void handle(MouseEvent event) {	
+		            	if(event.getButton().toString().equals("SECONDARY")){
+		            		System.out.println("rechte maustaste clicked");
+		            		int newRadius = (int) ((event.getX() - MyCircle.this.getCenterX() + event.getY() - MyCircle.this.getCenterY())/2);
+		            		MyCircle.this.setRadius(newRadius);
+		            		
+		            		System.out.println("new Radius: "+newRadius);
+		            		
+		            	}else{
+		            		MyCircle.this.setCenterX(event.getX());
+			            	MyCircle.this.setCenterY(event.getY());
+		            	}
+		            }
+		        });
 	}
 	
 
--- Graphics Editor/src/objects/MySquare.java
+++ Graphics Editor/src/objects/MySquare.java
@@ -2,7 +2,10 @@ package objects;
 
 import interfaces.Drawable;
 import interfaces.IShape;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class MySquare extends Rectangle implements IShape,Drawable,Cloneable {
@@ -39,12 +42,6 @@ public class MySquare extends Rectangle implements IShape,Drawable,Cloneable {
 		this.setWidth(a);
 		this.setHeight(a);
 	}
-
-	@Override
-	public void setHandler() {
-		// TODO Auto-generated method stub
-		
-	}
 	
 	public double getxCoord() {
 		return xCoord;
@@ -60,6 +57,51 @@ public class MySquare extends Rectangle implements IShape,Drawable,Cloneable {
 
 	public void setyCoord(double yCoord) {
 		this.yCoord = yCoord;
+	}
+	
+	@Override
+	public void setHandler() {
+		this.setOnMouseClicked(new EventHandler<MouseEvent>(){
+		 
+		            @Override
+		            public void handle(MouseEvent event) {
+		            	if(event.getButton().toString().equals("SECONDARY")){
+		            		
+		            	}else{
+			            	if(MySquare.this.isFocused()){
+			            		MySquare.this.setStrokeWidth(0);
+			            		MySquare.this.setFocused(false);
+			            	}else{
+			            		MySquare.this.setStroke(new Color(1, 0, 0, 1));
+			            		MySquare.this.setStrokeWidth(2);
+			            		MySquare.this.setFocused(true);
+			            	}
+		            	}
+		            	
+		            	System.out.println("Rectangle OnClick");
+		            	
+		            }
+		        });
+		
+		this.setOnMouseDragged(new EventHandler<MouseEvent>(){
+		 
+		            @Override
+		            public void handle(MouseEvent event) {	
+		            	if(event.getButton().toString().equals("SECONDARY")){
+		            		System.out.println("rechte maustaste clicked");
+		            		int newWidth = (int) ((event.getX() - MySquare.this.getX() + event.getY() - MySquare.this.getX())/2);
+		            		MySquare.this.resize(newWidth, newWidth);
+		            		MySquare.this.setWidth(newWidth);
+		            		MySquare.this.setHeight(newWidth);
+		            		
+		            		System.out.println("width= "+MySquare.this.getWidth() +" hieght = "+MySquare.this.getHeight());
+		            		
+		            	}else{
+		            		MySquare.this.setX(event.getX());
+			            	MySquare.this.setY(event.getY());
+		            	}
+		            }
+		        });
 	}
 	
 }
